b83d0deff25e1058244ef14ab3479a64ccef173b
Sidhart <sidharth1295@gmail.com>
2016-05-05 13:36:43 +0530

    Added files via upload

---
+++ RxWithObsAv.ino
@@ -0,0 +1,328 @@
+/*******Wirelessly Controlled Rover using Arduino and RF Modules************
+*************Coded and tested successfully by Sidharth Makhija******
+**************https://droidhangar.wordpress.com/contact/***********
+******************** Version 1 28/2/2014***************************
+*********************************************************************
+*********************************************************************/    
+#include <Ultrasonic.h> 
+#include <VirtualWire.h>
+#include <AFMotor.h>
+
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ);
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ); // create motor #2, 64KHz pwm
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+
+
+
+int distLeft = 0;
+int distRight = 0;
+int distFront = 0;
+int trigerpin = 14;
+int echopin = 15; 
+                    
+int distLimit = 30;    //distance limit for obstacle = 30cm       
+int readDistane();
+unsigned long pulsetime = 0;  
+unsigned long distance = 0;   
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("setup");
+
+    // Initialise the IO and ISR
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);	 // Bits per sec
+  vw_set_rx_pin(2);
+  vw_rx_start();       
+ 
+  motor1.setSpeed(200);
+  motor2.setSpeed(200); 
+  motor3.setSpeed(200);
+  motor4.setSpeed(200);
+ 
+
+
+  pinMode(trigerpin, OUTPUT);  
+  pinMode(echopin, INPUT);     
+          
+  stopMotor();               
+  Serial.println("STOP");    
+  delay(2000);                 
+  
+}
+
+
+void loop()
+{
+
+ uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+	int i;
+
+        digitalWrite(13, true); // Flash a light to show received good message
+	// Message with a good checksum received, dump it.
+	
+	
+	for (i = 0; i < buflen; i++)
+	{
+	    
+        if(buf[i] == '1')
+      {
+        forward();
+        Serial.println("forward");
+      }
+    if(buf[i] == '2')
+      {
+        backward();
+        Serial.println("backward");
+      }
+        if(buf[i] == '4')
+      {
+        left();
+        Serial.println("left");
+      }
+        if(buf[i] == '3')
+      {
+        right();
+        Serial.println("right");
+      }
+        if(buf[i] == '5')
+       {
+         stopMotor();
+         Serial.println("stopped");
+       }
+       if(buf[i] == '6')
+       {
+         ObstacleAvoiderMode();
+         Serial.println("Obstacle Avoidance Selected");
+       }
+	    Serial.print(" ");
+	}
+	Serial.println("");
+        digitalWrite(13, false); 
+        
+    }
+  } 
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void ObstacleAvoiderMode()
+{
+  int distLimit = 30; 
+   distLeft = 0;
+   distRight = 0;
+   distFront = 0;
+  
+  
+  Serial.println("processing.");  
+  delay(1000); 
+
+                 
+  distFront = readDistance();  
+  Serial.print("DistF: ");     
+  Serial.print(distFront);
+  Serial.println(" cm.");  
+  delay(100);
+
+  distLeft = readLeft();
+  Serial.print("DistL: ");     
+  Serial.print(distLeft);
+  Serial.println(" cm.");  
+  delay(100);
+  
+  distRight = readRight();
+  Serial.print("DistR: ");     
+  Serial.print(distRight);
+  Serial.println(" cm.");  
+  delay(100);
+    
+ /* 
+  myservo.write(180);             
+  delay(1000);                    
+  distLeft = readDistance();   
+  Serial.print("DistL: ");     
+  Serial.print(distLeft);
+  Serial.println(" cm.");
+  
+  myservo.write(90);           
+  delay(1000);                 
+  distFront = readDistance();  
+  Serial.print("DistF: ");     
+  Serial.print(distFront);
+  Serial.println(" cm.");
+  
+  
+  myservo.write(0);               
+  delay(1000);                     
+  distRight = readDistance();    
+  Serial.print("DistR: ");        
+  Serial.print(distRight);
+  Serial.println(" cm.");
+  
+  myservo.write(90);
+  delay(1000);*/                       
+  if(distFront<distLimit)
+  {
+  if(distFront < distLeft && distFront < distRight)
+  {
+    if(distLeft==0 && distRight==1 && distLeft>distFront)
+    {
+      Serial.println("turning left");     
+      left();                        
+      delay(100);
+    }
+    else
+   
+   {
+      if(distLeft == distRight && distLeft == 1 && distRight == 1)                 // Case in which the distance left and right are equal
+      { 
+        Serial.println("Avoiding");  
+        backward();
+        delay(500);
+        
+      }
+   
+      else if(distRight == 0 && distLeft == 1 && distRight > distFront)
+      {
+        Serial.println("turning right");    
+        right();                     
+        delay(100);
+      }
+    }
+  }
+  }
+  else  {
+    forward();
+  }
+
+  
+  distLeft = 0;
+  distRight = 0;
+  distFront = 0;
+                                              // robot goes forward until he detects an object at a distance less than the distance limit
+  int x = 1;                               
+  while(x)
+  {                                
+    Serial.println("in front");          
+    forward();                                //moves forward
+    distFront = readDistance();           
+    Serial.print("DistF: ");               
+    Serial.print(distFront);
+    Serial.println(" cm.");
+    if(distFront < distLimit)                // if the distance is less than the limit distance
+    {           
+       stopMotor (); 
+      delay(200);                          
+      x = 0;                               
+                                            
+                                           
+
+      Serial.println("stationary");
+}
+
+  }
+  }
+
+void forward()
+{
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  
+}
+
+void backward()
+{
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+}
+
+void right()
+{
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+}
+
+void left()
+{
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+
+void stopMotor()
+{
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+}
+
+  
+
+int readDistance()
+{
+  
+  digitalWrite(trigerpin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigerpin, HIGH);       
+  delayMicroseconds(10);               
+  digitalWrite(trigerpin,LOW);         
+  pulsetime = pulseIn(echopin, HIGH);  
+  return pulsetime/58.2;                
+                                       
+}
+
+int readRight()
+{
+  int reading = digitalRead(16);
+  if(reading==1)
+  {
+    distRight = 0;
+  Serial.println("blocked");
+  }
+  else
+  {
+    distRight = distFront + 30;
+  Serial.println("unblocked");
+  }
+   
+  delay(100);
+}
+
+int readLeft()
+{
+  int reading = digitalRead(17);
+  if(reading==1)
+  {
+    distLeft = 0;
+  Serial.println("blocked");
+  
+  }
+  else
+  {
+    distLeft = distFront + 30;
+  Serial.println("unblocked");
+  }
+  
+  delay(100);
+}
+
+//end
---
+++ TransmitterWithHeadLights.ino
@@ -0,0 +1,228 @@
+/********Wirelessly Controlled Rover using Arduino and RF Modules
+*************Coded and tested successfully by Sidharth Makhija***********
+******************** Version 1 28/2/2014***************************
+
+Working : This is the Transmitter code of the Rover.
+
+The four directions for the rover to move are given via the joystick,
+there is also a brake button..but that isn't necessarily needed
+since the rover does not move until the joystick is moved.
+
+The additional button is used to switch the headlights on and off.
+
+This code uses the VirtualWire and AFMotor(motorshield library).
+
+The motor shield used is self soldered by using Adafruit's motorshield
+schemaitic which they have provided on their site since they're awesome!
+
+More pictures,videos and info on the rover here  :
+http://letsmakerobots.com/node/40002
+http://www.instructables.com/id/The-WirelessObstacle-DetectingBeeping-Robot-using-/
+
+For any doubts,corrections,etc. shoot a mail here :
+https://droidhangar.wordpress.com/contact/
+*******************************************************************************
+*********************************************************************
+*/
+
+#include <VirtualWire.h>
+
+//Initialize the variables
+int UD = 0;
+int LR = 0;
+int brake = 16;
+int vert = 0;
+int hor = 0;
+
+int hLight = 4;
+//Variable to keep track of the number of button presses.
+int counter = 1;
+
+void setup() 
+{
+  Serial.begin(9600);//Only for Debugging
+  
+  pinMode(hLight,INPUT);
+  pinMode(brake,INPUT);
+  
+  digitalWrite(12,HIGH);
+  digitalWrite(brake,HIGH);
+  digitalWrite(hLight,HIGH);
+  
+  //Start Transmitting	
+  startTx();  
+       
+}
+
+void loop()
+{
+  //Reads the joytick movement
+  JoystickReader();
+  
+  //Character that will be sent
+  char *msg2;
+  
+  //Conditions when joystick is stationary
+  if(vert == 5 && hor == 4 )
+  {
+    char *msg2 = "5";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+    //Debugging
+    Serial.println("brake");
+    Serial.println("  ");
+  }
+  
+  //When joystick is moved forward.
+  if(hor==4 && vert<5)
+  {
+    char *msg2 = "1";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+   //Debugging 
+    Serial.println("Forward");
+    Serial.println("  ");
+  }
+  
+  //When joystick is moved backward.
+  if(hor==4 && vert>5)
+  {
+    char *msg2 = "2";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+    //Debugging
+    Serial.println(" Backward ");
+    Serial.println("  ");
+  }
+  
+  //When joytick is moved right.
+  if(vert==5 && hor<4)
+  {
+    char *msg2 = "3";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+    
+    Serial.println("Right ");
+    Serial.println("  ");
+  }
+  
+  //When joystick is moved left.
+  if(vert==5 && hor>4)
+  {
+   char *msg2 = "4";
+   digitalWrite(13, true); // Flash a light to show transmitting
+   vw_send((uint8_t *)msg2, strlen(msg2));
+   vw_wait_tx(); // Wait until the whole message is gone
+   digitalWrite(13, false);
+    
+    Serial.println("Left ");
+    Serial.println("  ");
+  }
+  
+  //When joystick button is pressed.
+  if(digitalRead(brake)==LOW) 
+   {
+    char *msg2 = "5";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+    
+    Serial.println("Brake");
+    Serial.println("  ");
+   }
+   
+   //Function that checks if the headlight button is pressed.
+   headLights();
+   
+}
+
+/////////////////////////Functions!Functions!Functions!//////////
+
+void JoystickReader()
+{
+  /*This function reads the analog values given by the joystick
+  and translates it as movement.*/
+  
+  /*Note : The horizontal and vertical of your joystick depends on how you have
+  soldered it on to the pcb...adjust as needed*/
+  
+  //Up Down
+  UD = analogRead(A1);
+  //Left Right
+  LR = analogRead(A0);
+  
+  /*This maps out the reading of the joystick from 0-1023 to 0-10
+  This makes it easier to work with while converting the readings 
+  into a direction in the if else statements. */
+  
+  hor = map(UD,0,1023,0,10);
+  vert = map(LR,0,1023,0,10);
+  
+ 
+}
+
+void startTx()
+{
+  
+  Serial.println("setup");
+  vw_setup(2000);
+  //Tx at digital pin 3	 
+  vw_set_tx_pin(3); 
+}
+
+void headLights()
+{
+  /*This function give the headlight on or off command based on the 
+  number of button presses.
+  
+  The counter variable is the number of times the headlight button is pressed.
+  
+  I plan on improving this part of the code by using interrupts as
+  this method isn't 100% accurate right now.
+  
+  */
+  
+  if(digitalRead(hLight)==LOW)
+  {
+    //Increase the value of counter every time the headlight button is pressed.
+    counter++;
+  }
+  
+  //If value of counter is even the Lights On command is sent.
+  if(counter%2==0)
+  {
+    char *msg2 = "6";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+    //Debugging
+    Serial.println("Lights On");
+    Serial.println("  ");
+  }
+  
+  //If counter is odd the Light Off command is sent.
+  else
+  {
+    char *msg2 = "7";
+      digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg2, strlen(msg2));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+    
+    Serial.println("Lights OFF");
+    Serial.println("  ");
+  }
+  delay(10);
+}
+
+//End
